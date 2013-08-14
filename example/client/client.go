add70c5bcae3c033d6693082359ea58b0427fdd4
Brian Picciano <bgp2396@bellsouth.net>
2013-08-13 23:47:12 -0400

    added example code

---
+++ example/client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"ghost"
+	"time"
+	"log"
+)
+
+const SERVER = "localhost:4000"
+
+type Hello struct {
+	A string
+	B int
+}
+
+func main() {
+	ghost.Register(Hello{})
+	ghost.AddConn(SERVER)	
+	for {
+		time.Sleep(1 * time.Second)
+
+		log.Println("Sending to server")
+		msg := Hello{"This is A", 2}
+		err := ghost.Send(SERVER, msg)
+		if err != nil {
+			log.Println("ERR",err)
+		}
+	}
+}
---
+++ example/server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"ghost"
+	"log"
+)
+
+type Hello struct {
+	A string
+	B int
+}
+
+func main() {
+	ghost.Register(Hello{})
+
+	rcvCh,errCh,err := ghost.Listen(":4000")
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	log.Println("Listening")
+
+	go func(){
+		for err := range errCh {
+			log.Println(err.Error())
+		}
+	}()
+
+	go func(){
+		for msg := range rcvCh {
+			msgW := (*msg).(Hello)
+			log.Printf("Got Hello message: %v",msgW)
+		}
+	}()
+
+	select {}
+}
